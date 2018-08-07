4c6c63e33e0d4cfa756cf3dc2a1fe7e92267e4ec
hattiephillips <harriet-phillips@hotmail.co.uk>
2018-08-07 05:57:53 -0400

    adding import requirements to server and simple app.get

--- httpsServer.js
+++ httpsServer.js
@@ -21,3 +21,23 @@ for (var i = 0; i < configarray.length; i++) {
 var split = configarray[i].split(':');
 config[split[0].trim()] = split[1].trim();
 }
+var pg = require('pg');
+var pool = new pg.Pool(config);
+
+app.get('postgistest', function (req,res) {
+pool.connect(function(err,client,done) {
+if(err){
+console.log("not able to get connection "+ err);
+res.status(400).send(err);
+}
+client.query('SELECT name FROM united_kingdom_counties'
+,function(err,result) {
+done();
+if(err){
+console.log(err);
+res.status(400).send(err);
+}
+res.status(200).send(result.rows);
+});
+});
+});
